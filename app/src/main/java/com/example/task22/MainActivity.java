37af8620edd707598d383561312f7b1e66e5439f
babamyrat <-l>
2021-06-29 11:11:29 +0300

    code 1

--- app/src/main/java/com/example/task22/MainActivity.java
+++ app/src/main/java/com/example/task22/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.task22;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn_click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btn_click = findViewById(R.id.btn_click);
+
+        btn_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello world!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
     }
 }
